ee576309a717a206fbea08d95c5c01716f66752e
Zhang Shuai <zhang.shuai@rea-group.com>
2015-06-15 17:35:10 +0800

    Add some background methods to load data and update data.

--- app/src/main/java/com/thoughtworks/xianbicycle/SearchResultActivity.java
+++ app/src/main/java/com/thoughtworks/xianbicycle/SearchResultActivity.java
@@ -1,9 +1,12 @@
 package com.thoughtworks.xianbicycle;
 
 import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
@@ -18,9 +21,27 @@ import butterknife.InjectView;
 
 public class SearchResultActivity extends AppCompatActivity {
 
+    public static final int UPDATE_LISTVIEW = 1;
+
     @InjectView(R.id.listview)
     ListView mListView;
     private SearchResultAdapater mSearchResultAdapater;
+    private List<BicycleSet> resultList;
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case UPDATE_LISTVIEW:
+                    mSearchResultAdapater.setResult(resultList);
+                    mSearchResultAdapater.notifyDataSetChanged();
+                    Toast.makeText(SearchResultActivity.this, msg.obj.toString(), Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+                    super.handleMessage(msg);
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,19 +55,113 @@ public class SearchResultActivity extends AppCompatActivity {
     }
 
     private void loadData(final String searchStart) {
+        search0();
+    }
+
+    private void search0() {
         new AsyncTask<Void, Void, List<BicycleSet>>() {
 
             @Override
             protected List<BicycleSet> doInBackground(Void... params) {
                 String result = FileUtil.readRawTextFile(SearchResultActivity.this, R.raw.result);
-                return new Gson().fromJson(result, new TypeToken<List<BicycleSet>>(){}.getType());
+                return new Gson().fromJson(result, new TypeToken<List<BicycleSet>>() {
+                }.getType());
             }
 
             @Override
-            protected void onPostExecute(List<BicycleSet> list) {
-                mSearchResultAdapater.setResult(list);
+            protected void onPostExecute(List<BicycleSet> bicycleSets) {
+                mSearchResultAdapater.setResult(bicycleSets);
                 mSearchResultAdapater.notifyDataSetChanged();
             }
         }.execute();
+    }
+
+    private void search1() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String result = FileUtil.readRawTextFile(SearchResultActivity.this, R.raw.result);
+                resultList = new Gson().fromJson(result, new TypeToken<List<BicycleSet>>() {
+                }.getType());
+
+                SearchResultActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mSearchResultAdapater.setResult(resultList);
+                        mSearchResultAdapater.notifyDataSetChanged();
+                    }
+                });
+            }
+        }).start();
+    }
+
+    private void search2() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String result = FileUtil.readRawTextFile(SearchResultActivity.this, R.raw.result);
+                resultList = new Gson().fromJson(result, new TypeToken<List<BicycleSet>>() {
+                }.getType());
+
+                SearchResultActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mSearchResultAdapater.setResult(resultList);
+                        mSearchResultAdapater.notifyDataSetChanged();
+                    }
+                });
+
+                Message msg = new Message();
+                msg.what = UPDATE_LISTVIEW;
+                msg.arg1 = 123;
+                msg.obj = "123456";
+
+                mHandler.obtainMessage();
+                msg.setTarget(mHandler);
+                msg.sendToTarget();
+
+//                mHandler.sendMessageDelayed(msg, 2000);
+//                mHandler.sendEmptyMessage(UPDATE_LISTVIEW);
+            }
+        }).start();
+
+    }
+
+    private void search3() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String result = FileUtil.readRawTextFile(SearchResultActivity.this, R.raw.result);
+                resultList = new Gson().fromJson(result, new TypeToken<List<BicycleSet>>() {
+                }.getType());
+
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mSearchResultAdapater.setResult(resultList);
+                        mSearchResultAdapater.notifyDataSetChanged();
+                    }
+                });
+            }
+        }).start();
+    }
+
+    private void search4() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String result = FileUtil.readRawTextFile(SearchResultActivity.this, R.raw.result);
+                resultList = new Gson().fromJson(result, new TypeToken<List<BicycleSet>>() {
+                }.getType());
+
+                mListView.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mSearchResultAdapater.setResult(resultList);
+                        mSearchResultAdapater.notifyDataSetChanged();
+                    }
+                });
+            }
+        }).start();
     }
 }
